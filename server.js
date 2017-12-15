1f510d0b72b9d45fa62ea08f5c2a6ee9c24bb54b
Raaj <raaja847@gmail.com>
2017-12-15 18:14:47 +0000

    Final code to get handlebars working

--- server.js
+++ server.js
@@ -8,8 +8,18 @@ const app = express();
 // TODO: Research the difference between using the method on line 11 and 13 because EJS only uses line 12
 
 // Configuring handlebars to use Express
-app.engine('handlebars', handlebars({ defaultLayout : main }));
+
+// The defaultLayout is main. Look in /views/layouts/main.handlebars
+app.engine('handlebars', handlebars({ defaultLayout : 'main' }));
 
 app.set('view engine', 'handlebars');
 
+// Setting up route to /
 
+app.get('/', (req, res) => {
+  res.render('index'); //Index file in /views
+})
+
+app.listen(5000, () => {
+  console.log('App started on localhost:5000');
+})
